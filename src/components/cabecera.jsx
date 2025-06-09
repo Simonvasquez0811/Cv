68c1e75729d99ae748e296ccb0e1850affc7d58d
Jorman Villada <jormanv2006@gmail.com>
2025-06-09 13:08:21 -0500

    cambio para usar props en cabecera

--- src/components/cabecera.jsx
+++ src/components/cabecera.jsx
@@ -1,21 +1,12 @@
-import "./cabecera.css";
+// src/components/CabeceraCV.jsx
+import React from "react";
 
-const user = {
-  nombre: "Simon Vasquez Escobar",
-  Cargo: "Desarrollador Front-End Jr",
-  correo: "simonvasquez550@gmail.com",
-  ubicacion: "Medellín",
-  telefono: "300 123 4567",
-};
-
-export default function CabeceraCV() {
+export default function CabeceraCV({ nombre, profesion }) {
   return (
     <header>
-      <h1>{user.nombre}</h1>
-      <h2>{user.Cargo}</h2>
-      <p>Correo: {user.correo}</p>
-      <p>Ubicación: {user.ubicacion}</p>
-      <p>Teléfono: {user.telefono}</p>
+      <h1>{nombre}</h1>
+      <h2>{profesion}</h2>
+      <hr />
     </header>
   );
 }
