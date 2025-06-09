6ca440a3148ff766c8a5ca30648fcdba2e52b412
Jorman Villada <jormanv2006@gmail.com>
2025-06-09 13:13:16 -0500

    Cambios en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,50 @@
-import CabeceraCV from "./components/cabecera.jsx";
-import Perfil from "./components/perfil.jsx";
-import Educacion from "./components/educacion.jsx";
-import Experiencia from "./components/experiencia.jsx";
-import StackTecnologias from "./components/StackTecnologias.jsx";
+// src/App.jsx
+import React from "react";
+import {
+  cabecera,
+  perfil,
+  educacion,
+  experiencia,
+  stackTecnologias,
+  proyectos,
+  habilidades
+} from "./Data/cvData";
+
+import CabeceraCV from "./components/cabecera";
+
+import Perfil from "./components/Perfil";
+import Educacion from "./components/Educacion";
+import Experiencia from "./components/Experiencia";
+import StackTecnologias from "./components/StackTecnologias";
+import Proyectos from "./components/Proyectos";
+import Habilidades from "./components/Habilidades";
 
 function App() {
   return (
-    <div>
-      <CabeceraCV />
-      <Perfil />
-      <Educacion />
-      <Experiencia />
-      <StackTecnologias />
+    <div style={{ maxWidth: "800px", margin: "0 auto", padding: "20px" }}>
+      {/* Cabecera */}
+      <CabeceraCV
+        nombre={cabecera.nombre}
+        profesion={cabecera.profesion}
+      />
+
+      {/* Perfil */}
+      <Perfil texto={perfil.texto} />
+
+      {/* Educación */}
+      <Educacion estudios={educacion} />
+
+      {/* Experiencia */}
+      <Experiencia trabajos={experiencia} />
+
+      {/* Stack de Tecnologías */}
+      <StackTecnologias tecnologias={stackTecnologias} />
+
+      {/* Proyectos (nuevo componente) */}
+      <Proyectos proyectos={proyectos} />
+
+      {/* Habilidades (nuevo componente) */}
+      <Habilidades habilidades={habilidades} />
     </div>
   );
 }
